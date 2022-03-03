99c60f2183aa2698a087627abdd603e2a24f50a5
sarahTarablus <sarahtarablus@gmail.com>
2022-03-02 16:13:51 -0800

    ui started

---
+++ RealTimeChatApp/ClientApp/src/components/ChatPage.js
@@ -0,0 +1,29 @@
+﻿import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
+import '../custom.css';
+
+const ChatPage = () => {
+
+
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-3 bg-info">
+                    <h5 className="title">Users</h5>
+                </div>
+                <div className="col-9 bg-light">
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="Text here" aria-label="Text here" aria-describedby="basic-addon2">
+                         </input>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        );
+    
+}
+
+export default ChatPage;
